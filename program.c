2debe446e757d13c0c6d96a21d742bfc5c255898
solene46 <solene.hasbrouck@esme.fr>
2025-09-26 08:10:36 +0000

    programe activité 2

--- program.c
+++ program.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main ()
+{
+    double wind_angle;
+    int wind_speed;
+    int fuel;
+    int consumption;
+    int wing_surface;
+    int pressure;
+    int temperature;
+    int altitude;
+    int ias;
+    int weight;
+
     return 0;
 }
