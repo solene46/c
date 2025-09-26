203908fdced945f6107842119236659431e64992
solene46 <solene.hasbrouck@esme.fr>
2025-09-26 08:27:30 +0000

    étape 1

--- program.c
+++ program.c
@@ -14,5 +14,36 @@ int main ()
     int ias;
     int weight;
 
+    printf("entrer Direction relative du vent:");
+    scanf ("%lf", &wind_angle);
+
+    printf("entrer vitesse vent:");
+    scanf ("%d", &wind_speed);    
+
+    printf("entrer carburant disponible:");
+    scanf ("%d", &fuel);   
+    
+    printf("entrer consommation:");
+    scanf ("%d", &consumption);  
+
+    printf("entrer surface alaire:");
+    scanf ("%d", &wing_surface);  
+
+    printf("entrer pression ambiante:");
+    scanf ("%d", &pressure);  
+    
+    printf("entrer Température extérieure:");
+    scanf ("%d", &temperature);    
+
+    printf("entrer altitude:");
+    scanf ("%d", &altitude);    
+
+    printf("entrer Indicated Air Speede:");
+    scanf ("%d", &ias); 
+
+    printf("entrer poids total:");
+    scanf ("%d", &weight); 
+
+    
     return 0;
 }
