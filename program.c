59677e74874630eddaa19e8b5d1c90e2378f1695
solene46 <solene.hasbrouck@esme.fr>
2025-10-10 08:45:10 +0000

    etape 2 commencée

--- program.c
+++ program.c
@@ -1,8 +1,47 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 int main ()
 {
+    bool b_wind_angle= false ;
+    bool b_wind_speed= false ;
+    bool b_fuel_= false ;
+    bool b_consumption = false ;
+    bool b_wing_surface = false ;
+    bool b_pressure = false ;
+    bool b_temperature = false ;
+    bool b_altitude = false ;
+    bool b_ias = false ;
+    bool b_weight = false ;
+    bool b_headwind = false ;
+    bool b_pressure_altitude = false ;
+    bool b_tas = false ;
+    bool b_ground_speed = false ;
+    bool b_range = false ;
+    bool b_takeoff =  false ;
+    bool b_wing_loading = false ;
+    bool b_rate_of_climb = false ;
+    bool b_return = false ;
+    bool b_descent_speed = false;
+
+// faire ca pour chaque variable //
+//tu sélectionne par exemple printf et apres tu fais controle+d pour marquer sur plusieurs lignes en meme temps des variable pour gagner du temps //
+
+    int choix; 
+    
+    printf("1- altitude-pression\n 2- Vitesse vraie\n 3-Vitesse-sol\n 4- Distance franchissable\n 5- Distance de décollage\n 6- Charge alaire\n 7- Taux de montée\n 8- Point de non-retour\n  9- Vitesse de descente\n ");
+    printf ("choisir:");
+    scanf ("%d",&choix);
+
+if(choix==1) {
+    b_pressure_altitude = true;
+    b_pressure = true;
+    b_altitude = true;
+}
+
+if (choix==2)
+
     double wind_angle;
     int wind_speed;
     int fuel;
@@ -35,8 +74,11 @@ int main ()
     printf("entrer Température extérieure:");
     scanf ("%d", &temperature);    
 
-    printf("entrer altitude:");
-    scanf ("%d", &altitude);    
+    if(b_altitude){
+        printf("entrer altitude:");
+        scanf ("%d", &altitude);    
+
+    }
 
     printf("entrer Indicated Air Speede:");
     scanf ("%d", &ias); 
@@ -44,19 +86,43 @@ int main ()
     printf("entrer poids total:");
     scanf ("%d", &weight); 
 
-    double headwind=wind_speed*cos(wind_angle);
-    double pressure_altitude=altitude+(1023-pressure)*30;
-    double tas=ias*(1+2*pressure_altitude/1000);
-    double ground_speed=tas - headwind;
-    double range=fuel*ground_speed*1.852/consumption;
-    double takeoff=	300*(1+pressure_altitude/1000)*(1-0.01*fmax(0, temperature-15))*(1-headwind/ground_speed)*(weight*weight/1157*1157);
-    double wing_loading=weight/wing_surface;
-    double rate_of_climb=700*(1-pressure_altitude/10000)*(1-0.01*fmax(0,temperature-15));
-    double no_return=range/2;
-    double descent_speed=ground_speed*tan(3);
+    double headwind;
+    
+    headwind = wind_speed*cos(wind_angle);
+    double pressure_altitude;
+    
+    if(b_pressure_altitude)
+    {
+        pressure_altitude = altitude+(1023-pressure)*30;
+        printf("Altitude-pression=%lf\n", pressure_altitude);
+    }
+        double tas;
+    
+    tas = ias*(1+2*pressure_altitude/1000);
+    double ground_speed;
+    
+    ground_speed = tas - headwind;
+    double range;
+    
+    range = fuel*ground_speed*1.852/consumption;
+    double takeoff;
+    
+    takeoff = 	300*(1+pressure_altitude/1000)*(1-0.01*fmax(0, temperature-15))*(1-headwind/ground_speed)*(weight*weight/1157*1157);
+    double wing_loading;
+    
+    wing_loading = weight/wing_surface;
+    double rate_of_climb;
+    
+    rate_of_climb = 700*(1-pressure_altitude/10000)*(1-0.01*fmax(0,temperature-15));
+    double no_return;
+    
+    no_return = range/2;
+    double descent_speed;
+    
+    descent_speed = ground_speed*tan(3);
 
     printf ("Vent de face=%lf\n", headwind);
-    printf("Altitude-pression=%lf\n", pressure_altitude);
+    
     printf ("Vitesse vraie=%lf\n", tas);
     printf ("Vitesse-sol=%lf\n", ground_speed);
     printf ("Distance franchissable=%lf\n", range);
